a99cca9300973fe41ced48246061e09a5849a72c
Mr-Robot-101 <git.sujon@gmail.com>
2022-11-28 23:28:03 +0600

    Api are created for seller and buyer

--- index.js
+++ index.js
@@ -25,21 +25,19 @@ const run = async () => {
     .db("indirect")
     .collection("productsCollection");
   const usersCollection = client.db("indirect").collection("usersCollection");
-  const bookingCollection = client.db("indirect").collection("bookingCollection");
+  const bookingCollection = client
+    .db("indirect")
+    .collection("bookingCollection");
   const blogsCollection = client.db("indirect").collection("blogsCollection");
   const cityCollection = client.db("indirect").collection("cityCollection");
-  const areaUnderCityCollection = client.db("indirect").collection("areaUnderCityCollection");
-
+  const areaUnderCityCollection = client
+    .db("indirect")
+    .collection("areaUnderCityCollection");
 
   try {
-
-
-
-
     // ..............................................................................
-   // Catagories
+    // Catagories
     // ..............................................................................
-
 
     // get single Catagories
     app.get("/catagories/:id", async (req, res) => {
@@ -55,7 +53,7 @@ const run = async () => {
       res.send(result);
     });
 
-      // ..............................................................................
+    // ..............................................................................
     // Product
     // ..............................................................................
 
@@ -67,84 +65,81 @@ const run = async () => {
     });
 
     app.get("/products", async (req, res) => {
-      let query={}
-      const category = req.query.category
-      const email = req.query.email
+      let query = {};
+      const category = req.query.category;
+      const email = req.query.email;
 
-      
-      if(email){
-        query= ({ email: email})
+      if (email) {
+        query = { email: email };
       }
-      
-    
-      if(category) {
-        query=({category : category})
+
+      if (category) {
+        query = { category: category };
       }
-      
+
       const result = await productsCollection.find(query).toArray();
       res.send(result);
     });
 
-    app.post('/products', async(req, res)=> {
-      let product = req.body
-      
-      product ={
+    app.post("/products", async (req, res) => {
+      let product = req.body;
+
+      product = {
         ...product,
-        Timestamp: new Date()
-      }
+        Timestamp: new Date(),
+      };
 
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
 
-      const result = await productsCollection.insertOne(product)
-      res.send(result)
-    })
+    // Delete product
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
 
-
-    // Delete product 
-    app.delete('/products/:id', async(req, res)=>{
-      const id=req.params.id
-      const query= {_id: ObjectId(id)}
-      const result= await productsCollection.deleteOne(query)
-      res.send(result)
-    })
-
-    // Update one for Advertizing 
-    app.put('/products/:id', async(req, res)=>{
-      const id=req.params.id
-      const filter= {_id: ObjectId(id)}
-      const toggle= req.body
+    // Update one for Advertizing
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const toggle = req.body;
 
       const updateDoc = {
         $set: {
-          isAdvertized: toggle
+          isAdvertized: toggle,
           // isAdvertized: {$not: isAdvertized}
         },
       };
- 
+
       const options = { upsert: true };
-      const result= await productsCollection.updateOne(filter, updateDoc, options)
-      res.send(result)
-    })
-
-
+      const result = await productsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
 
     // Updating all Products value
 
-    app.get('/productsUpdate', async(req, res)=> {
-
-      const options= {upsert : true}
+    app.get("/productsUpdate", async (req, res) => {
+      const options = { upsert: true };
       const updateValue = {
-        $set:{
+        $set: {
+          accountType: "Seller",
+        },
+      };
 
-          accountType: "Seller"
-        }
-      }
-    
-      const result = await productsCollection.updateMany({}, updateValue, options)
-      res.send(result)
-    })
-
-
-  
+      const result = await productsCollection.updateMany(
+        {},
+        updateValue,
+        options
+      );
+      res.send(result);
+    });
 
     // ..............................................................................
     // Booking Information
@@ -157,35 +152,30 @@ const run = async () => {
       res.send(bookings);
     });
 
-app.get('/bookings', async(req, res)=>{
-  const buyerEmail = req.query.buyerEmail;
-  console.log(buyerEmail)
-  let query = {}
-  if(buyerEmail){
-    query= {buyerEmail: buyerEmail}
-  }
-  const bookings= await bookingCollection.find(query).toArray()
-  res.send(bookings)
-})
+    app.get("/bookings", async (req, res) => {
+      const buyerEmail = req.query.buyerEmail;
+      console.log(buyerEmail);
+      let query = {};
+      if (buyerEmail) {
+        query = { buyerEmail: buyerEmail };
+      }
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    });
 
+    app.post("/bookings", async (req, res) => {
+      let booking = req.body;
 
-app.post('/bookings', async(req, res)=>{
-  let booking= req.body
+      if (booking) {
+        booking = {
+          ...booking,
+          Timestamp: new Date(),
+        };
+      }
 
-  if(booking) {
-    booking = {
-      ...booking, 
-      Timestamp: new Date()
-    }
-  }
-
-  const result = await bookingCollection.insertOne(booking)
-  res.send(result)
-})
-
-
-
-
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
 
     // ..............................................................................
     // User
@@ -203,25 +193,61 @@ app.post('/bookings', async(req, res)=>{
     // get all users
     app.get("/users", async (req, res) => {
       const email = req.query.email;
-      // const decodedEmail = req.decoded.email;
-      // console.log(decodedEmail)
+      const accountType = req.query.accountType;
 
-    //   if (email !== decodedEmail) {
-    //     return res.status(403).send({ message: "forbidden access" });
-    //   }
+      let query = {};
 
-    let  query = {}
-    if(email) {
-       query = { email: email };
-    }
-     
+      if (email) {
+        query = { email: email };
+      }
+
+    
 
       const users = await usersCollection.find(query).toArray();
-
 
       res.send(users);
     });
 
+    // get all users
+    app.get("/sellers", async (req, res) => {
+      const email = req.query.email;
+      const accountType = req.query.accountType;
+
+      let query = {};
+
+      if (accountType) {
+        query = { accountType: "Seller" };
+      }
+
+      const users = await usersCollection.find(query).toArray();
+
+      res.send(users);
+    });
+    // get all users
+    app.get("/Buyers", async (req, res) => {
+      const email = req.query.email;
+      const accountType = req.query.accountType;
+
+      let query = {};
+
+      if (accountType) {
+        query = { accountType: "Buyer" };
+      }
+
+      const users = await usersCollection.find(query).toArray();
+
+      res.send(users);
+    });
+
+
+
+
+
+
+
+
+
+    
     //   users post
 
     app.post("/users", async (req, res) => {
@@ -230,49 +256,78 @@ app.post('/bookings', async(req, res)=>{
       res.send(result);
     });
 
-
-    app.put('/users/:id', async(req, res)=>{
-      const id=req.params.id
-      const filter= {_id: ObjectId(id)}
-      const toggle= req.body
-      
+    app.put("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const toggle = req.body;
+      // productUserVerifing(id, toggle)
 
       const updateDoc = {
         $set: {
-          isVerified: toggle
-          
+          isVerified: toggle,
         },
       };
- 
+
       const options = { upsert: true };
-      const result= await usersCollection.updateOne(filter, updateDoc, options)
-      res.send(result)
-    })
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
 
+      res.send(result);
+    });
 
-    // Delete user 
+    // const productUserVerifing  = (userId, toggle) => {
+    //   app.get("/products", async (req, res) => {
+    //     const query= req.query.isVerified
+    //     console.log(query)
+    //     console.log(userId, toggle)
+    //     // const filter= {_id: ObjectId(id)}
 
-      // ..............................................................................
+    //     // const updateDoc = {
+    //     //   $set: {
+    //     //     isVerified: toggle
+
+    //     //   },
+    //     // };
+
+    //     // const options = { upsert: true };
+    //     // const result= await productsCollection.updateOne(filter, updateDoc, options)
+    //     // res.send(result)
+    //   })
+    // }
+
+    // Delete user
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // ..............................................................................
     // Blog Posts
     // ..............................................................................
-    app.get('/blogs/:id', async(req, res)=> res.send(await blogsCollection.findOne({_id : ObjectId(req.params.id)})))
+    app.get("/blogs/:id", async (req, res) =>
+      res.send(await blogsCollection.findOne({ _id: ObjectId(req.params.id) }))
+    );
 
+    app.get("/blogs", async (req, res) =>
+      res.send(await blogsCollection.find({}).toArray())
+    );
 
-
-    app.get('/blogs', async(req,res)=> res.send(await blogsCollection.find({}).toArray()))
-
-    app.get('/updateBlogs', async(req, res) => {
-      const options= {upsert : true}
+    app.get("/updateBlogs", async (req, res) => {
+      const options = { upsert: true };
       const updateValue = {
-        $set:{
-
-          Timestamp: new Date()
-        }
-      }
-      const result =await blogsCollection.updateMany({}, updateValue, options)
-      res.send(result)
-    })
-
+        $set: {
+          Timestamp: new Date(),
+        },
+      };
+      const result = await blogsCollection.updateMany({}, updateValue, options);
+      res.send(result);
+    });
   } finally {
   }
 };
